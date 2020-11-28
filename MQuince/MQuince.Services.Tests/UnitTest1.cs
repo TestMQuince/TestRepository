d1f00193155c6e2acc1eb8941d4f6dda2ba7f8bc
Dusan Petrovic <dusan.petrovic@vega.local>
2020-11-28 22:46:09 +0100

    test tests

---
+++ MQuince/MQuince.Services.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace MQuince.Services.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(false);
+        }
+
+        [Fact]
+        public void Test21()
+        {
+            Assert.True(true);
+        }
+    }
+}
